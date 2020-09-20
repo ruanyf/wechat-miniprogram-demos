4c27120260a3b8a9e1859dcbba8c1ec49bca0974
ruanyf <yifeng.ruan@gmail.com>
2020-09-20 20:31:34 +0800

    feat: add data-binding demo

---
+++ demos/data-binding/README.md
@@ -0,0 +1,3 @@
+window 对象配置
+
+https://developers.weixin.qq.com/miniprogram/dev/reference/configuration/app.html
---
+++ demos/data-binding/pages/home/home.js
@@ -0,0 +1,8 @@
+const app = getApp();
+
+Page({
+  data: {
+    name: '张三',
+    now: app.globalData.now
+  }
+});
